5d7564c8204deda919973444a5988d98315ef5c0
MaxiZ <1934922335@qq.com>
2024-09-23 22:56:48 +0800

    数字反转

--- 09231.cpp
+++ 09231.cpp
@@ -1,6 +1,29 @@
+// 数字反转
 #include <stdio.h>
+#include <iostream>
+using namespace std;
 int main(void)
 {
-    printf("hh");
+    int num, s = 0;
+    cin >> num;
+    if (num < 0)
+    {
+        num = -num;
+        for (; num > 0;)
+        {
+            s = s * 10 + num % 10;
+            num /= 10;
+        }
+        printf("-%d", s);
+    }
+    else
+    {
+        for (; num > 0;)
+        {
+            s = s * 10 + num % 10;
+            num /= 10;
+        }
+        printf("%d", s);
+    }
     return 0;
 }
